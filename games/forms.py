70a07d5d89dafaff13a0a93a5de15f8ee8efad32
eunasin7 <eunasin7@gmail.com>
2026-07-20 21:08:42 +0900

    feat: 반격 및 게임 취소 기능 구현

--- games/forms.py
+++ games/forms.py
@@ -23,3 +23,21 @@ class GameCreateForm(forms.ModelForm):
         self.fields["attacker_card"].choices = [
             (card, str(card)) for card in card_options
         ]
+
+
+class GameCounterForm(forms.ModelForm):
+    defender_card = forms.TypedChoiceField(
+        label="카드",
+        coerce=int,
+        widget=forms.RadioSelect,
+    )
+
+    class Meta:
+        model = Game
+        fields = ("defender_card",)
+
+    def __init__(self, *args, card_options, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["defender_card"].choices = [
+            (card, str(card)) for card in card_options
+        ]
--- games/urls.py
+++ games/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path("create/", views.game_create, name="create"),
     path("history/", views.game_history, name="history"),
     path("<int:pk>/", views.game_detail, name="detail"),
+    path("<int:pk>/counter/", views.game_counter, name="counter"),
+    path("<int:pk>/cancel/", views.game_cancel, name="cancel"),
 ]
 
 
